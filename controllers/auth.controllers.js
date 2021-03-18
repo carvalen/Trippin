fca0f75a6761f8b0cbfdfddd0838c7e3a27514d6
Tomas Yaya <tomasyaya@Tomass-MacBook-Pro.local>
2021-03-18 19:03:05 +0100

    solve auth issues

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,22 +1,14 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcryptjs");
 
-
-
-
 exports.signup = async (req, res) => {
   try {
-    console.log("usuarioooo");
     const { username, password, email } = req.body;
     const hasMissingCredentials = !username || !password || !email;
-    console.log("body", req.body)
+
     if (hasMissingCredentials) {
       return res.status(400).json({ message: "Missing credentials" });
     }
-
-    // if (!hasCorrectPasswordFormat(password)) {
-    //   return res.status(400).json({ message: "Incorrect password format" });
-    // }
 
     const user = await User.findOne({ email });
 
@@ -27,19 +19,22 @@ exports.signup = async (req, res) => {
     const saltRounds = 10;
     const salt = await bcrypt.genSalt(saltRounds);
     const hashedPassword = await bcrypt.hash(password, salt);
+    console.log("hashedPassword", hashedPassword);
     const newUser = await User.create({ username, email, hashedPassword });
 
-    // req.session.userId = newUser._id;
+    console.log(req.session);
+    req.session.userId = newUser._id;
 
     return res.status(200).json({ user: newUser.email, id: newUser._id });
   } catch (e) {
-    if (isMongooseErrorValidation(e)) {
-      return res.status(400).json({ message: "Incorrect email format" });
-    }
-    if (isMongoError(e)) {
-      return res.status(400).json({ message: "Duplicate field" });
-    }
-    return res.status(400).json({ message: "Wrong request" });
+    // if (isMongooseErrorValidation(e)) {
+    //   return res.status(400).json({ message: "Incorrect email format" });
+    // }
+    // if (isMongoError(e)) {
+    //   return res.status(400).json({ message: "Duplicate field" });
+    // }
+    console.log("error", e);
+    return res.status(400).json({ message: "Wrong request", error: e });
   }
 };
 
@@ -51,11 +46,8 @@ exports.login = async (req, res) => {
       return res.status(400).json({ message: "Missing credentials" });
     }
 
-    if (!hasCorrectPasswordFormat(password)) {
-      return res.status(400).json({ message: "Incorrect password format" });
-    }
-
     const user = await User.findOne({ email });
+    console.log("user", user);
     if (!user) {
       return res.status(400).json({ message: "User does not exist" });
     }
@@ -66,15 +58,16 @@ exports.login = async (req, res) => {
     if (!hasCorrectPassword) {
       return res.status(401).json({ message: "Unauthorize" });
     }
-
+    console.log("session", req.session);
     req.session.userId = user._id;
 
     return res.status(200).json({ user: user.email, id: user._id });
   } catch (e) {
-    if (isMongooseErrorValidation(e)) {
-      return res.status(400).json({ message: "Incorrect email format" });
-    }
-    return res.status(400).json({ message: "Wrong request" });
+    // if (isMongooseErrorValidation(e)) {
+    //   return res.status(400).json({ message: "Incorrect email format" });
+    // }
+    console.log("error", e);
+    return res.status(400).json({ message: "Wrong request", error: e });
   }
 };
 
@@ -83,8 +76,8 @@ exports.logout = async (req, res) => {
   res.status(200).json({ message: "logout" });
 };
 
-  exports.getUser = async (req, res) => {
-    const { userId } = req.session;
-    const { email, _id } = await User.findOne(userId);
-   res.status(200).json({ id: _id, email });
-  };
+exports.getUser = async (req, res) => {
+  const { userId } = req.session;
+  const { email, _id } = await User.findOne(userId);
+  res.status(200).json({ id: _id, email });
+};
